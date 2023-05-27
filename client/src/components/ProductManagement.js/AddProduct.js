640a363812db25d495eb688ff467f6bd056373ca
User <michaelm810129@gmail.com>
2023-05-27 19:19:00 +0930

    added delete update component structure

--- client/src/components/ProductManagement.js/AddProduct.js
+++ client/src/components/ProductManagement.js/AddProduct.js
@@ -18,7 +18,6 @@ const AddProduct = () => {
       margin: 30,
       border: "1px solid",
       borderRadius: 20,
-      color: "white",
     },
   };
 
@@ -33,12 +32,11 @@ const AddProduct = () => {
   const [category, setCategory] = useState("");
   const [quantity, setQuantity] = useState("");
   const [price, setPrice] = useState("");
-  const[successMessage, setSuccessMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState("");
   // const [image, setImage] = useState("");
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    
 
     try {
       const formattedPrice = parseFloat(price);
@@ -54,20 +52,19 @@ const AddProduct = () => {
         },
       });
       console.log(addProduct);
-      setSuccessMessage(`${productName}` + "successfully added to the database!");
-
-     
+      setSuccessMessage(
+        `${productName}` + " successfully added to the database!"
+      );
 
       // console.log(data);
-      
+
       setTimeout(() => {
         setProductName("");
         setDescription("");
         setCategory("");
         setQuantity("");
         setPrice("");
-        setSuccessMessage('');
-
+        setSuccessMessage("");
       }, 3000);
     } catch (error) {
       console.log(error);
@@ -83,77 +80,75 @@ const AddProduct = () => {
 
   return (
     <div style={styles.div}>
-    <div>
-      <h2>Add Product</h2>
-    </div>
-    <div className="w-50">
-      <form onSubmit={handleFormSubmit}  className="form">
-        <Form.Label htmlFor="inputrProductName">Product Name</Form.Label>
-        <Form.Control
-          type="input"
-          id="inputProductName"
-          aria-describedby="inputProductName"
-          name="productName"
-          onChange={(event) => setProductName(event.target.value)}
-          value={productName}
-        />
-        <Form.Text id="inputProductName" muted></Form.Text>
-        <Form.Label htmlFor="inputDescription">Description</Form.Label>
-        <Form.Control
-          type="input"
-          id="inputDescription"
-          aria-describedby="inputDescription"
-          onChange={(event) => setDescription(event.target.value)}
-          value={description}
-        />
-        <Form.Text id="inputProductPrice" muted></Form.Text>
-        <Form.Label htmlFor="inputProductPrice">Price</Form.Label>
-        <Form.Control
-          type="number"
-          id="inputProductPrice"
-          aria-describedby="inputProductPrice"
-          onChange={(event) => setPrice(event.target.value)}
-          value={price}
-        />
-        <Form.Label htmlFor="inputQuantity">Quantity</Form.Label>
-        <Form.Control
-          type="number"
-          id="inputQuantity"
-          aria-describedby="inputQuantity"
-          onChange={(event) => setQuantity(event.target.value)}
-          value={quantity}
-        />
-        <Form.Label htmlFor="inputNewCategory">Choose Category</Form.Label>
-        <Form.Select
-          aria-label="Default select example"
-          onChange={(event) => setCategory(event.target.value)}
-          value={category}
-        >
-          <option>Select Category to add new product to:</option>
-          {queryLoading ? (
-            <div>Loading...</div>
-          ) : (
-            allCategory.map((categories) => (
-              <option key={categories._id} value={categories._id}>
-                {categories.categoryName}
-              </option>
-            ))
-          )}
-        </Form.Select>
-        <Button style={styles.button} variant="success" type="submit">
-          Add Product
-        </Button>{" "}
-      </form>
-    </div>
-    {error && (
-      <div className="col-12 my-3 bg-danger text-white p-3">
-        Something went wrong...
+      <div>
+        <h2>Add Product</h2>
       </div>
-      
-    )}
-    {successMessage && <div style={{ color: 'black'}}>{successMessage}</div>}
-  </div>
-  
+      <div className="w-50">
+        <form onSubmit={handleFormSubmit} className="form">
+          <Form.Label htmlFor="inputrProductName">Product Name</Form.Label>
+          <Form.Control
+            type="input"
+            id="inputProductName"
+            aria-describedby="inputProductName"
+            name="productName"
+            onChange={(event) => setProductName(event.target.value)}
+            value={productName}
+          />
+          <Form.Text id="inputProductName" muted></Form.Text>
+          <Form.Label htmlFor="inputDescription">Description</Form.Label>
+          <Form.Control
+            type="input"
+            id="inputDescription"
+            aria-describedby="inputDescription"
+            onChange={(event) => setDescription(event.target.value)}
+            value={description}
+          />
+          <Form.Text id="inputProductPrice" muted></Form.Text>
+          <Form.Label htmlFor="inputProductPrice">Price</Form.Label>
+          <Form.Control
+            type="number"
+            id="inputProductPrice"
+            aria-describedby="inputProductPrice"
+            onChange={(event) => setPrice(event.target.value)}
+            value={price}
+          />
+          <Form.Label htmlFor="inputQuantity">Quantity</Form.Label>
+          <Form.Control
+            type="number"
+            id="inputQuantity"
+            aria-describedby="inputQuantity"
+            onChange={(event) => setQuantity(event.target.value)}
+            value={quantity}
+          />
+          <Form.Label htmlFor="inputNewCategory">Choose Category</Form.Label>
+          <Form.Select
+            aria-label="Default select example"
+            onChange={(event) => setCategory(event.target.value)}
+            value={category}
+          >
+            <option>Select Category to add new product to:</option>
+            {queryLoading ? (
+              <div>Loading...</div>
+            ) : (
+              allCategory.map((categories) => (
+                <option key={categories._id} value={categories._id}>
+                  {categories.categoryName}
+                </option>
+              ))
+            )}
+          </Form.Select>
+          <Button style={styles.button} variant="success" type="submit">
+            Add Product
+          </Button>{" "}
+        </form>
+      </div>
+      {error && (
+        <div className="col-12 my-3 bg-danger text-white p-3">
+          Something went wrong...
+        </div>
+      )}
+      {successMessage && <div style={{ color: "black" }}>{successMessage}</div>}
+    </div>
   );
 };
 
--- client/src/components/ProductManagement.js/DeleteProduct.js
+++ client/src/components/ProductManagement.js/DeleteProduct.js
@@ -1,14 +1,82 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const styles = {
+  div: {
+    padding: 30,
+    margin: 30,
+    border: "1px solid",
+    borderRadius: 20,
+  },
+  // textArea: {
+  //   margin: 5,
+  // },
+  button: {
+    // float: "right",
+    padding: 5,
+    margin: 5,
+  },
+};
 
 const DeleteProduct = () => {
+  const handleFormSubmit = async () => {
+    try {
+    } catch (error) {
+      throw new Error("Error handling form submit: " + error);
+    }
+  };
 
-
-    return (
+  return (
+    <>
+      <div style={styles.div}>
         <div>
-            <h2>Delete Product</h2>
+          <h2>Delete Product</h2>
+          <p>
+            WARNING!: Once a Product has been deleted, it will be lost forever.
+          </p>
         </div>
-    )
-
+        <div>
+          <form
+            className="form"
+            type="form"
+            name="deleteForm"
+            onSubmit={handleFormSubmit}
+          >
+            <Form.Select
+              style={styles.textArea}
+              //   className="w-50"
+              aria-label="Default select example"
+              name="productId"
+              //   value={}
+              //   onChange={}
+            >
+              <option>Choose a Category to update</option>
+              {/* {loadingQuery ? (
+                <div>Loading...</div>
+              ) : (
+                allCategory.map((categories) => (
+                  <option key={categories._id} value={categories._id}>
+                    {categories.categoryName}
+                  </option>
+                ))
+              )}
+              ; */}
+            </Form.Select>
+            <Button style={styles.button} variant="danger" type="submit">
+              DELETE Product
+            </Button>{" "}
+          </form>
+          {/* {error && (
+            <div className="col-12 my-3 bg-danger text-white p-3">
+              Something went wrong...
+            </div>
+          )} */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DeleteProduct;
